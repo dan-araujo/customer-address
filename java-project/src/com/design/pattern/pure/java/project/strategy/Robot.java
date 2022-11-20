5656c287368b71dff387633ccf63285fbda77b40
dan-araujo <daniel.ti.info@gmail.com>
2022-11-20 16:00:49 -0300

    Class Robot with Strategy

---
+++ java-project/src/com/design/pattern/pure/java/project/strategy/Robot.java
@@ -0,0 +1,15 @@
+package com.design.pattern.pure.java.project.strategy;
+
+public class Robot {
+
+	private Behavior behavior;
+
+	public void setBehavior(Behavior behavior) {
+		this.behavior = behavior;
+	}
+
+	public void move() {
+		behavior.move();
+
+	}
+}
